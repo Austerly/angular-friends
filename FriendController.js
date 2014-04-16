9917f6c3ae93b130bfc37fda7c024403f0d21a9f
Austin Hesterly <austin.hesterly@gmail.com>
2014-04-16 12:58:52 -0600

    finished day 1

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,13 @@
-
-
+angular.module('apper').controller('FriendController', function($scope){
+	//Everything happens here:
+	$scope.test = "Hello World";
+	$.getJSON('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json', function(myJsonDataFromServer){
+		$scope.friends = myJsonDataFromServer.results;
+		$scope.$apply(function(){
+			$scope.friends = my.results;
+		});
+	});
+});
 
 
 
