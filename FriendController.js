c7e0f6528641fd4743090068646ee76b84456e19
Austin Hesterly <austin.hesterly@gmail.com>
2014-04-16 20:25:06 -0600

    Completed Day 2
    Completed day 2, still needs debugging and black diamond

--- FriendController.js
+++ FriendController.js
@@ -1,13 +1,19 @@
-angular.module('apper').controller('FriendController', function($scope){
+angular.module('apper').controller('FriendController', function($scope, $http){
 	//Everything happens here:
 	$scope.test = "Hello World";
-	$.getJSON('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json', function(myJsonDataFromServer){
-		$scope.friends = myJsonDataFromServer.results;
-		$scope.$apply(function(){
-			$scope.friends = my.results;
+	$scope.scender = false;
+	$scope.sorter = 'name';
+	// $.getJSON('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json', function(myJsonDataFromServer){
+	// 	$scope.friends = myJsonDataFromServer.results;
+	// 	$scope.$apply(function(){
+	// 		$scope.friends = my.results;
+	// 	});
+	// });
+	$http({method: 'GET', 
+		url: 'https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json'}).
+		success(function(data){
+			$scope.friends = data.results;
+			$scope.friendName = friends.name;
 		});
-	});
+
 });
-
-
-
